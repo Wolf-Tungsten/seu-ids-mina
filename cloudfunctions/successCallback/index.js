65496e5d456e7716e954db2edfcb6ff338da9de0
Wolf-Tungsten <canis.tungsten@foxmail.com>
2019-01-25 00:27:04 +0800

    完成callback云函数，修改部分规则

--- cloudfunctions/successCallback/index.js
+++ cloudfunctions/successCallback/index.js
@@ -1,16 +1,26 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
-
+const axios = require('axios')
 cloud.init()
 
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
-
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
+  const {callbackUrl, name, cardnum, identity, ids_session} = event // 解析参数
+  let attempt = 0
+  while(attempt < 3){
+    try{
+      let res = await axios.post(callbackUrl, {cardnum, name, ids_session, identity}, {timeout:5000, responseType: 'arraybuffer'})
+      if(res.data.toString('utf8').indexOf('success') !== -1) {
+        return true // 成功返回真
+      } else {
+        break // 响应不包含 success 视为失败
+      }
+    } catch(e) {
+      // 超时或出错则重试，最多重试三次
+      attempt++
+    }
   }
+  // 重试三次后依然失败则返回假
+  return false
 }
